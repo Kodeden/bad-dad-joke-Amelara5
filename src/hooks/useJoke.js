9c4b814e918c75f379d19dbff5e8a456e8a49073
Andres Melara <andres.melara.t@gmail.com>
2023-02-01 18:27:30 -0500

    Add useJoke

---
+++ src/hooks/useJoke.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+function useJoke() {
+  const [joke, setJoke] = useState("");
+  const [goodJokes, setGoodJokes] = useState([]);
+  const [badJokes, setBadJokes] = useState([]);
+
+  const handleClick = (event) => {
+    if (event.target.textContent.includes("&#x1F44D;&#127997")) {
+      setGoodJokes((jokeList) => [...jokeList, joke]);
+    } else if (event.target.textContent.includes("&#x1F44E;&#127997")) {
+      setBadJokes((jokeList) => [...jokeList, joke]);
+    }
+    setJoke("");
+  };
+
+  return { joke, setJoke, goodJokes, badJokes, handleClick };
+}
+
+export default useJoke;
