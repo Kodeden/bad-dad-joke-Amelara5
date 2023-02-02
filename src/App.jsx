4397511e8bb43f500650c632406ef65b11a13dda
Andres Melara <andres.melara.t@gmail.com>
2023-02-02 12:55:48 -0500

    Use tailwinds classes
    Rearrange app so buttons appear after pressing joke button

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,38 @@
 import JokeButton from "./components/JokeButton";
 import JokeList from "./components/JokeList";
 import JokeText from "./components/JokeText";
-import JokeVote from "./components/JokeVote";
 import useJoke from "./hooks/useJoke";
 
 function App() {
   const { joke, setJoke, goodJokes, badJokes, handleClick } = useJoke();
 
   return (
-    <main>
+    <main className=" flex h-screen flex-col items-center justify-center bg-dark-sienna text-white">
       <JokeButton setJoke={setJoke} />
-      <JokeText joke={joke} />
+      <JokeText joke={joke} handleClick={handleClick} />
 
-      <JokeVote handleClick={handleClick} message={"Funny joke 👍🏽👍🏾"} />
-      <JokeList listOfJokes={goodJokes} />
+      <main className="flex gap-2">
+        <section className="max-w-md basis-1/2">
+          <h2 className=" mx-2 min-w-full rounded-lg bg-dark-red py-3 text-center text-bright-yellow-crayola">
+            Dad certified 😎
+          </h2>
+          <JokeList listOfJokes={goodJokes} />
+        </section>
 
-      <JokeVote handleClick={handleClick} message={"Bad joke 👎🏽👎🏾"} />
-      <JokeList listOfJokes={badJokes} />
+        <section className="max-w-md basis-1/2">
+          <h2 className=" mx-2 min-w-full rounded-lg bg-dark-red py-3 text-center text-bright-yellow-crayola">
+            Keep trying 🙄
+          </h2>
+          <JokeList listOfJokes={badJokes} />
+        </section>
+      </main>
     </main>
   );
 }
-
+// Made me giggle
+// I felt nothing
+// Dad certified 😎
+// Keep trying 🙄
+// 👍🏽 Funny joke 👍🏾
+// 👎🏽 Bad joke 👎🏾
 export default App;
