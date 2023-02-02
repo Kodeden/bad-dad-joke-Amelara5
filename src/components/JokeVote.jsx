9efa3c1c8a0b7c129db42d8e3a4effb70340eb46
Andres Melara <andres.melara.t@gmail.com>
2023-02-01 22:51:14 -0500

    Added JokeVote

--- src/components/JokeVote.jsx
+++ src/components/JokeVote.jsx
@@ -1,3 +1,21 @@
-function JokeVote() {}
+import PropTypes from "prop-types";
+
+function JokeVote({ handleClick, message }) {
+  return (
+    <>
+      <button
+        className="rounded bg-blue-500 px-4 py-2 text-white"
+        onClick={handleClick}
+      >
+        {message}
+      </button>
+    </>
+  );
+}
+
+JokeVote.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+  message: PropTypes.string.isRequired,
+};
 
 export default JokeVote;
