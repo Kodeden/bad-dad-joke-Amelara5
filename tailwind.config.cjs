0472841c562edff393cc4af95c1accc8f4b20a8e
Andres Melara <andres.melara.t@gmail.com>
2023-02-02 12:52:43 -0500

    Add color palette to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.jsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-sienna": "#220901",
+        "blood-red": "#621708",
+        "dark-red": "#941B0C",
+        rust: "#BC3908",
+        "bright-yellow-crayola": "#F6AA1C",
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
