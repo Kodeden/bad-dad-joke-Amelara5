81e2e3b59739788a26602fa3d38cddd3e5df5cc1
Andres Melara <andres.melara.t@gmail.com>
2023-02-02 13:09:25 -0500

    Move the css from app to JokeList

--- src/App.jsx
+++ src/App.jsx
@@ -10,29 +10,11 @@ function App() {
     <main className=" flex h-screen flex-col items-center justify-center bg-dark-sienna text-white">
       <JokeButton setJoke={setJoke} />
       <JokeText joke={joke} handleClick={handleClick} />
-
       <main className="flex gap-2">
-        <section className="max-w-md basis-1/2">
-          <h2 className=" mx-2 min-w-full rounded-lg bg-dark-red py-3 text-center text-bright-yellow-crayola">
-            Dad certified 😎
-          </h2>
-          <JokeList listOfJokes={goodJokes} />
-        </section>
-
-        <section className="max-w-md basis-1/2">
-          <h2 className=" mx-2 min-w-full rounded-lg bg-dark-red py-3 text-center text-bright-yellow-crayola">
-            Keep trying 🙄
-          </h2>
-          <JokeList listOfJokes={badJokes} />
-        </section>
+        <JokeList title={"Dad certified 😎"} listOfJokes={goodJokes} />
+        <JokeList title={"Keep trying 🙄"} listOfJokes={badJokes} />
       </main>
     </main>
   );
 }
-// Made me giggle
-// I felt nothing
-// Dad certified 😎
-// Keep trying 🙄
-// 👍🏽 Funny joke 👍🏾
-// 👎🏽 Bad joke 👎🏾
 export default App;
--- src/components/JokeList.jsx
+++ src/components/JokeList.jsx
@@ -1,22 +1,30 @@
 import PropTypes from "prop-types";
 
-function JokeList({ listOfJokes }) {
+function JokeList({ title, listOfJokes }) {
   return (
-    <ul>
-      {listOfJokes.map((joke) => {
-        return (
-          <li key={joke.id} className="px-6 text-lg font-bold">
-            {`${joke.setup} `}
-            <span className=" text-sm font-normal text-rust">
-              {joke.punchline}
-            </span>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <section className="max-w-md basis-1/2">
+        <h2 className=" mx-2 min-w-full rounded-lg bg-dark-red py-3 text-center text-bright-yellow-crayola">
+          {title}
+        </h2>
+        <ul>
+          {listOfJokes.map((joke) => {
+            return (
+              <li key={joke.id} className="px-6 text-lg font-bold">
+                {`${joke.setup} `}
+                <span className=" text-sm font-normal text-rust">
+                  {joke.punchline}
+                </span>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </>
   );
 }
 JokeList.propTypes = {
+  title: PropTypes.string.isRequired,
   listOfJokes: PropTypes.arrayOf(
     PropTypes.shape({
       setup: PropTypes.string.isRequired,
