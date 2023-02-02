a32dafdd8c5c969c35d6bdd2107adf5f00359e1d
Andres Melara <andres.melara.t@gmail.com>
2023-02-01 23:05:27 -0500

    Fixed prop warning and identifier of button

--- src/hooks/useJoke.js
+++ src/hooks/useJoke.js
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
 function useJoke() {
-  const [joke, setJoke] = useState("");
+  const [joke, setJoke] = useState(null);
   const [goodJokes, setGoodJokes] = useState([]);
   const [badJokes, setBadJokes] = useState([]);
 
   const handleClick = (event) => {
-    if (event.target.textContent.includes("&#x1F44D;&#127997")) {
+    if (event.target.textContent.includes("Funny")) {
       setGoodJokes((jokeList) => [...jokeList, joke]);
-    } else if (event.target.textContent.includes("&#x1F44E;&#127997")) {
+    } else if (event.target.textContent.includes("Bad")) {
       setBadJokes((jokeList) => [...jokeList, joke]);
     }
     setJoke(null);
